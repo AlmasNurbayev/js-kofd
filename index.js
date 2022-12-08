02514419c2b3114f1515f5a48643ed251f0d9b78
almas <ntldr@mail.ru>
2022-12-08 13:47:15 +0600

    v03

--- index.js
+++ index.js
@@ -24,15 +24,22 @@ async function getJWT() {
     httpsAgent: agent,
   };
 
-  axios(config)
-    .then(function (response) {
-      let fs = require('fs');
-      fs.writeFile('responce.txt', JSON.stringify(response.data.data.jwt), error2 =>{});
-      return JSON.stringify(response.data.data.jwt);
-    })
-    .catch(function (error) {
-      return error;
-    });
+  console.log("1");
+
+  try {
+      const response = await axios(config);
+      console.log("2");
+      console.log(typeof(response));
+       let fs = require('fs');
+       fs.writeFile('response-post.txt', String(response.data.data.jwt), error2 =>{});
+      return String(response.data.data.jwt);
+    }
+    catch (error) {
+      console.log("3");
+       let fs = require('fs');
+       fs.writeFile('error-post.txt', String(error), error2 =>{});
+      return String(error);
+    };
 }
 //config.headers = {'Authorization': `Bearer ${tokenStr}`}
 
@@ -49,28 +56,33 @@ async function getData() {
     organizationXin: "800727301256",
   };
 
-  let token = `Bearer ${getJWT()}`;
-  console.log(token);
-  const config = {
-    method: "get",
-    url: 'https://cabinet.kofd.kz/api/operations?skip=0&take=20&requireTotalCount=true&sort=[{"selector":"operationDate","desc":true}]&cashboxId=33812',
-    headers: {
-      "Content-Type": "application/json",
-      'Authorization': token
-    },
-    data: JSON.stringify(data),
-    httpsAgent: agent,
-  };
+  let token = getJWT();
+  console.log(typeof(token));
+  let fs = require('fs');
+  fs.writeFile('jwt.txt', String(token), error2 =>{});
+
+  // const config = {
+  //   method: "get",
+  //   url: 'https://cabinet.kofd.kz/api/operations?skip=0&take=20&cashboxId=33812',
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     'Authorization': token
+  //   },
+  //   data: JSON.stringify(data),
+  //   httpsAgent: agent,
+  // };
 
 
 
-  try {
-    const res = await axios(config);
-    console.log(res.data)
+  // try {
+  //   const res = await axios(config);
+  //   console.log(res.data)
 
-  } catch (error) {
-    console.log(error);
-  }
+  // } catch (error) {
+  //   console.log(error);
+  //   let fs = require('fs');
+  //   fs.writeFile('error-get.txt', JSON.stringify(error), error2 =>{});
+  // }
 }
 
 console.log(getData());
