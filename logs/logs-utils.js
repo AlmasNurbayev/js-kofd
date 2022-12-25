f99203b09011bd0be63d2edb40cc4ff2644cfcca
almas <ntldr@mail.ru>
2022-12-25 21:21:53 +0600

    refactor log-utils/readlog

--- logs/logs-utils.js
+++ logs/logs-utils.js
@@ -1,9 +1,12 @@
 "use strict";
 
 const fs = require('fs/promises');
-const fs2 = require('fs');
-//const { resolve } = require('path');
 
+// write string  to text file
+// input name - name of file, 
+// input data - text of record, 
+// input append - write to end of old file or writing new file
+// return promise
 async function writeLog(name, data, append = true) {
   if (append) {
     await fs.appendFile('logs/'+name, JSON.stringify(data) + '\n');
@@ -12,6 +15,10 @@ async function writeLog(name, data, append = true) {
   };
 }
 
+// write string error to text file
+// input error - text of error, 
+// input point - description - place about function
+// return promise
 async function writeError(error, point) {
   await writeLog('errors.txt', {
     date: new Date(),
@@ -20,42 +27,45 @@ async function writeError(error, point) {
   });
 }
 
-async function readLog(name, numberRow) {
+// reading text file
+// input name - name of file, 
+// input countRow - count of last rows
+// return string - last rows in text file
+async function readLog(name, countRow) {
  
+  if (!await isFileExist('logs/'+name)) {
+    console.log('not found file: ' + 'logs/'+name);
+    writeError('not found file: ' + 'logs/'+name, 'readLog');
+    return '';
+  }
 
   try {
     const res = await fs.readFile('logs/'+name);
-    return res.toString().split('\n').slice(-numberRow).join('\n');
+    return res.toString().split('\n').slice(-countRow).join('\n');
   } catch (err) {
     // write error log
     writeError(err.stack, 'logs-utils-readLog');
   }
 }
 
-  // return new Promise(function (resolve, reject) {
-  //   fs2.readFile('logs/' + name, 'utf8', function(err, data) {
-  //     if (err) {
-  //       reject(err);
-  //       writeError(err.stack, 'logs-utils-readLog');
-  //     }
-  //     else {
-  //       let data3 = '';
-  //       let data2 = data.split('\n');
-  //       for (let i = 0; i < data2.length; i += 1) {
-  //         if (i > data2.length - numberRow - 1) {
-  //           //console.log(data2[i]);
-  //           data3 += data2[i] + '\n';
-  //         }
-  //       }
-  //       resolve(data3);
-  //     }
-  //   });
-  // });
-
-
 (async () => {
-  console.log(await readLog('bot_request.txt', 10));
+  //console.log(await readLog('bot_request.txt', 10));
 })();
+
+// check exist of file
+// input name - name-path of file, 
+// return true or false
+async function isFileExist(name) {
+  console.log(name);
+  const constants = require('fs');
+  try {
+    await fs.access(name, constants.R_OK);
+    return true;
+  } catch (error) {
+    // write error твой
+    return false;
+  }
+}
 
 exports.writeError = writeError;
 exports.writeLog = writeLog;
