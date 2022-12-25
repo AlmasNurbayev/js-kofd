9ce2dc00d0dcb9f67a0f25e7e6dc625e75894449
almas <ntldr@mail.ru>
2022-12-25 20:57:11 +0600

    minimized readLog in logs-utils

--- logs/logs-utils.js
+++ logs/logs-utils.js
@@ -22,33 +22,40 @@ async function writeError(error, point) {
 
 async function readLog(name, numberRow) {
  
-  return new Promise(function (resolve, reject) {
-    fs2.readFile('logs/' + name, 'utf8', function(err, data) {
-      if (err) {
-        reject(err);
-        writeError(err.stack, 'logs-utils-readLog');
-      }
-      else {
-        let data3 = '';
-        let data2 = data.split('\n');
-        for (let i = 0; i < data2.length; i += 1) {
-          if (i > data2.length - numberRow - 1) {
-            //console.log(data2[i]);
-            data3 += data2[i] + '\n';
-          }
-        }
-        resolve(data3);
-      }
-    });
-  });
+
+  try {
+    const res = await fs.readFile('logs/'+name);
+    return res.toString().split('\n').slice(-numberRow).join('\n');
+  } catch (err) {
+    // write error log
+    writeError(err.stack, 'logs-utils-readLog');
+  }
 }
 
+  // return new Promise(function (resolve, reject) {
+  //   fs2.readFile('logs/' + name, 'utf8', function(err, data) {
+  //     if (err) {
+  //       reject(err);
+  //       writeError(err.stack, 'logs-utils-readLog');
+  //     }
+  //     else {
+  //       let data3 = '';
+  //       let data2 = data.split('\n');
+  //       for (let i = 0; i < data2.length; i += 1) {
+  //         if (i > data2.length - numberRow - 1) {
+  //           //console.log(data2[i]);
+  //           data3 += data2[i] + '\n';
+  //         }
+  //       }
+  //       resolve(data3);
+  //     }
+  //   });
+  // });
+
+
 (async () => {
-  //console.log(await readLog('bot_request.txt', 10));
+  console.log(await readLog('bot_request.txt', 10));
 })();
-
-
-
 
 exports.writeError = writeError;
 exports.writeLog = writeLog;
