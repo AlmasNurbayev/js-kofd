73794c71fedcd026cc0c43d92829ba42ccccdcc9
almas <ntldr@mail.ru>
2022-12-25 16:33:14 +0600

    log-utils/readlog changed ot async mode

--- logs/logs-utils.js
+++ logs/logs-utils.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const fs = require('fs/promises');
-const fs_sync = require('fs');
+const fs2 = require('fs');
 //const { resolve } = require('path');
 
 async function writeLog(name, data, append = true) {
@@ -21,29 +21,30 @@ async function writeError(error, point) {
 }
 
 async function readLog(name, numberRow) {
-  let data3 = '';  
-  let data = fs_sync.readFileSync('logs/'+name, 'utf8') 
-  // , function (err, data)  {
-  //   console.log(data);  
-  //   if (err) {
-  //     console.log(err.stack);    
-  //     writeError(err.stack, 'logs-utils-readLog');
-  //   } 
-    
-    let data2 = data.split('\n');
-    for (let i = 0; i < data2.length; i += 1) {
-      // Этот код выполняется для каждого элемента
-
-      if (i > data2.length-numberRow-1) {
-          console.log(i);
-          data3 += data2[i] + '\n';
-      }  
-    }
-  return data3;
+ 
+  return new Promise(function (resolve, reject) {
+    fs2.readFile('logs/' + name, 'utf8', function(err, data) {
+      if (err) {
+        reject(err);
+        writeError(err.stack, 'logs-utils-readLog');
+      }
+      else {
+        let data3 = '';
+        let data2 = data.split('\n');
+        for (let i = 0; i < data2.length; i += 1) {
+          if (i > data2.length - numberRow - 1) {
+            //console.log(data2[i]);
+            data3 += data2[i] + '\n';
+          }
+        }
+        resolve(data3);
+      }
+    });
+  });
 }
 
 (async () => {
-//  console.log(await readLog('bot_request.txt', 10));
+  //console.log(await readLog('bot_request.txt', 10));
 })();
 
 
